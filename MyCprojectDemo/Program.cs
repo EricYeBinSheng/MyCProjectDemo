006523833b4321347b0d89ee229358a3e4d4407e
EricYeBinSheng <992706405@qq.com>
2019-03-08 16:09:54 +0800

    commit from feature branch

--- MyCprojectDemo/Program.cs
+++ MyCprojectDemo/Program.cs
@@ -15,6 +15,10 @@ namespace MyCprojectDemo
             Console.WriteLine("now is editting in dev branch and then merge to master branch");
 
             Console.WriteLine("up is wrong,because i forgot to commit ,the content is the same!");
+
+            Console.WriteLine("now test in feature branch ");
+
+
         }
     }
 }
