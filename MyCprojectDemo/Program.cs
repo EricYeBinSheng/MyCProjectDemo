da89c9a617feebeb430044acf57e3b81d98c43e9
EricYeBinSheng <992706405@qq.com>
2019-03-08 15:49:21 +0800

    commit from in dev first

--- MyCprojectDemo/Program.cs
+++ MyCprojectDemo/Program.cs
@@ -13,6 +13,8 @@ namespace MyCprojectDemo
             Console.WriteLine("this is my first project use github to manage");
 
             Console.WriteLine("now is editting in dev branch and then merge to master branch");
+
+            Console.WriteLine("up is wrong,because i forgot to commit ,the content is the same!");
         }
     }
 }
