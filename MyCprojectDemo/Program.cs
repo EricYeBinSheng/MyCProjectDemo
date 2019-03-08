2fadb3adfe9294cbdad8db964f72a85a25036fa1
EricYeBinSheng <992706405@qq.com>
2019-03-08 15:46:48 +0800

    this is a first edition commit in vs2017 from master branch

--- MyCprojectDemo/Program.cs
+++ MyCprojectDemo/Program.cs
@@ -11,6 +11,8 @@ namespace MyCprojectDemo
         static void Main(string[] args)
         {
             Console.WriteLine("this is my first project use github to manage");
+
+            Console.WriteLine("now is editting in dev branch and then merge to master branch");
         }
     }
 }
